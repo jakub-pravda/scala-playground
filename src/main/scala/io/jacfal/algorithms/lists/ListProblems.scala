0b6c7df5a2005ec582d4b326fcca525a13c66234
Jakub Pravda <jakub.pravda@comsource.cz>
2022-01-19 21:39:54 +0100

    list functionality implementation

--- src/main/scala/io/jacfal/algorithms/lists/ListProblems.scala
+++ src/main/scala/io/jacfal/algorithms/lists/ListProblems.scala
@@ -12,11 +12,18 @@ sealed abstract class MyList[+T] {
   def apply(index: Int): T
   def length: Int
   def reverse: MyList[T]
+  def ++[S >: T](anotherList: MyList[S]): MyList[S]
+  def removeAt(index: Int): MyList[T]
 }
 
 object MyList {
   def from[T](iter: Iterable[T]): MyList[T] = {
-    ???
+    @tailrec
+    def tooMyListRecursive(remaining: Iterable[T], acc: MyList[T]): MyList[T] = {
+      if (remaining.isEmpty) acc
+      else tooMyListRecursive(remaining.tail, remaining.head :: acc)
+    }
+    tooMyListRecursive(iter, MyNil).reverse
   }
 }
 
@@ -32,6 +39,10 @@ case object MyNil extends MyList[Nothing] {
   override def length: Int = 0
 
   override def reverse: MyList[Nothing] = MyNil
+
+  override def ++[S >: Nothing](anotherList: MyList[S]): MyList[S] = anotherList
+
+  override def removeAt(index: Int): MyList[Nothing] = MyNil
 }
 
 case class ::[+T](override val head: T, override val tail: MyList[T]) extends MyList[T] {
@@ -77,29 +88,65 @@ case class ::[+T](override val head: T, override val tail: MyList[T]) extends My
 
     reverseRec(this, MyNil)
   }
+
+  override def ++[S >: T](anotherList: MyList[S]): MyList[S] = {
+    @tailrec // complexity: 2*O(N)
+    def addRecursive(remaining: MyList[S], acc: MyList[S]): MyList[S] = {
+      if (remaining.isEmpty) acc
+      else addRecursive(remaining.tail, remaining.head :: acc)
+    }
+    addRecursive(this.reverse, anotherList)
+  }
+
+  override def removeAt(index: Int): MyList[T] = {
+    @tailrec // complexity: O(N)
+    def removeAtRecursive(remaining: MyList[T], acc: MyList[T], currentIndex: Int): MyList[T] = {
+      if(remaining.isEmpty) this // index out of bounds
+      else if (currentIndex == index) acc.reverse ++ remaining.tail
+      else removeAtRecursive(remaining.tail, remaining.head :: acc, currentIndex + 1)
+    }
+
+    if (index < 0) this
+    else removeAtRecursive(this, MyNil, 0)
+  }
 }
 
 object ListsProblems extends App {
-  val testList =  1 :: 2 :: 3 :: 4 :: MyNil
+  val testList1 =  1 :: 2 :: 3 :: 4 :: MyNil
+  val testList2 =  5 :: 6 :: 7 :: 8 :: MyNil
+  val largeList = MyList.from(1 to 1000)
   val shortList = 1 :: MyNil
   val emptyList = MyNil
 
   // search the k-th element
-  assert(testList(0) == 1)
-  assert(testList(3) == 4)
-  assert(Try(testList(-1)).isFailure)
-  assert(Try(testList(10)).isFailure)
+  assert(testList1(0) == 1)
+  assert(testList1(3) == 4)
+  assert(Try(testList1(-1)).isFailure)
+  assert(Try(testList1(10)).isFailure)
   println("Search k-th element tests OK")
 
   // list length test
-  assert(testList.length == 4)
+  assert(testList1.length == 4)
   assert(shortList.length == 1)
   assert(emptyList.length == 0)
   println("Length tests OK")
 
   // reverse list
-  assert(testList.reverse.toString == "[4, 3, 2, 1]")
+  assert(testList1.reverse.toString == "[4, 3, 2, 1]")
   assert(shortList.reverse.toString == "[1]")
-  assert(Try(emptyList.reverse.toString).isFailure)
+  assert(emptyList.reverse == MyNil)
   println("Reverse tests OK")
+
+  // list concatenate
+  assert((testList1 ++ testList2).toString == "[1, 2, 3, 4, 5, 6, 7, 8]")
+  assert((emptyList ++ testList1).toString == "[1, 2, 3, 4]")
+  assert((testList1 ++ emptyList).toString == "[1, 2, 3, 4]")
+  println("List concat tests OK")
+
+  // remove k-th element
+  assert(testList1.removeAt(1).toString == "[1, 3, 4]")
+  assert(testList1.removeAt(3).toString == "[1, 2, 3]")
+  assert(testList1.removeAt(10).toString == "[1, 2, 3, 4]")
+  assert(shortList.removeAt(0) == MyNil)
+  println("Remove k-th element test OK")
 }
